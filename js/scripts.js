1b870ce61b3b6351ed36b70b27c14a2e0930f779
Matheus Lombardi <matheus.profissional2022@gmail.com>
2025-02-27 20:15:54 -0300

    Excluindo Alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 function seguirPerfil(){
-    alert('Voce chegou no JS')
     const buttonSeguir = document.getElementById('seguir');
     if(buttonSeguir.innerHTML === 'Seguir perfil'){
         buttonSeguir.innerHTML = 'Você está seguindo este perfil';
