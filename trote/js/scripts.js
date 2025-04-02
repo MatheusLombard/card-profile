263b579e7d62d6a7e0227af0d8c71ff4372e1422
Matheus Lombardi <matheus.profissional2022@gmail.com>
2025-04-01 22:17:58 -0300

    atualizando script

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -2,17 +2,31 @@ function calcular() {
     //vamos criar duas variaveis 
     //js as variaveis não possuem tipo
     
-    let mascote, homenagem, leite, kitAvulso, suplementoAvulso;
+    let mascote, homenagem, leite, kit, suplemento, soma, equipe;
     
-    mascote = document.getElementById('mascote').value
-    homenagem = document.getElementById('homenagem').value
-    leite = document.getElementById('leite').value
-    kitAvulso = document.getElementById('kitAvulso').value
-    suplementoAvulso = document.getElementById('suplementoAvulso').value
+    mascote = Number(document.getElementById('mascote').value);
+    homenagem = Number(document.getElementById('homenagem').value);
+    leite = Number(document.getElementById('leite').value);
+    kit = Number(document.getElementById('kit').value);
+    suplemento = Number(document.getElementById('suplemento').value);
+
+    //Soma parcial
+    soma = mascote + homenagem + ((leite)* 2)  //+ ((suplemento) * 15);
     
-    let soma;
 
-    soma = Number(mascote) + Number(homenagem) + (Number(leite)* 2) + (Number(kitAvulso) * 30) + (Number(suplementoAvulso) * 15)
+    equipe = document.getElementById('equipe').value
+    
+    if(equipe === 'Laranja'){
+        if(kit >= 97){
+            soma += 5000 + ((kit - 97) * 30)
+        }else if (kit >= 78){
+            soma += 4000 + ((kit - 78) * 30)
+        }else if(kit >= 49){ 
+            soma += 2500 + ((kit - 49) * 30)
+        }else if(kit >= 19){
+            soma += 1000 + ((kit - 19) * 30)
+        }
+    }
 
-    document.getElementById("soma").innerHTML = `A soma é ${soma}`
+    document.getElementById("soma").innerHTML = `A soma é ${soma}`;
 }
